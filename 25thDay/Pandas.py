d8f94e126e0126b2579810f266df45d9f233fb8b
Aadarshrai1801 <aadarshrai1801@gmail.com>
2025-10-21 16:30:07 +0530

    .

--- 25thDay/Pandas.py
+++ 25thDay/Pandas.py
@@ -5,3 +5,21 @@ data = pandas.read_csv("25thDay\Weather_data.csv")
 print(data["temp"].mean())
 
 print(data["temp"].max())
+
+print(data[data["temp"] == data["temp"].max()])
+
+monday = (data[data["day"] == "Monday"])
+fer = monday.temp * 1.8 + 32
+
+print(fer)
+
+# Create a Dataframe from scratch
+
+data_dict = {
+    "Student" : ["Amy", "James", "Angela"],
+    "Marks" : [99, 34, 67]
+}
+
+data = pandas.DataFrame(data_dict)
+print(data)
+data.to_csv("Data_dict.csv")
