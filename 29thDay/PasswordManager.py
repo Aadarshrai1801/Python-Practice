05c3ccccff42dbb2485d2e69bea363cee49957d0
Aadarshrai1801 <aadarshrai1801@gmail.com>
2025-10-30 23:27:51 +0530

    test commit

--- 29thDay/PasswordManager.py
+++ 29thDay/PasswordManager.py
@@ -2,9 +2,52 @@
 
 from tkinter import *
 
+from tkinter import messagebox
+
+import random
+
+import pyperclip
+
 # PASSWORD GENERATOR 
 
+def generate_password() :
+
+  letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+  symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+  nr_letters = random.randint(8, 10)
+  nr_symbols = random.randint(2, 4)
+  nr_numbers = random.randint(2, 4)
+
+  password_letters = [random.choice(letters) for _ in range(nr_letters)]
+  password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+  password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+
+  password_list = password_letters + password_symbols + password_numbers
+
+  random.shuffle(password_list)
+
+  password = "".join(password_list)
+  password_entry.insert(0, password)
+  
+  pyperclip.copy(password)  
+
 # SAVE PASSWORD 
+
+def save_password():
+    
+    if len(website_entry.get()) == 0 or len(username_entry.get()) == 0 or len(password_entry.get()) == 0 :
+        messagebox.showinfo(title = "Error", message = "You have left any field empty.")
+    
+    else :
+        is_ok = messagebox.askokcancel(title = f"{website_entry.get()}", message = f"These are the details entered: \nEmail: {username_entry.get()}"f"\nPassword: {password_entry.get()} \nis it ok to save?")
+        
+        if is_ok :
+            with open("29thDay/Passwords.txt", "a") as file:
+                file.write(f"{website_entry.get()} | {username_entry.get()} | {password_entry.get()}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)  
 
 # UI SETUP
 
@@ -12,15 +55,46 @@ from tkinter import *
     
 window = Tk()
 window.title("Password Manager")
-window.minsize(350, 350)
+window.config(padx = 50, pady = 50)
 
     # Canvas
 
-canvas = Canvas(width=200, height=224, bg = window.cget("bg"))  
+canvas = Canvas(width = 200, height = 200, bg = window.cget("bg"))  
 img = PhotoImage(file = "29thDay/Logo.png")
 canvas.create_image(100, 100, image = img)
-canvas.pack(padx = 20, pady = 65)
+canvas.grid(row = 0, column = 1)
 
-print("Hello World")
+    # Label
+    
+website_label = Label(text = "Website:")
+website_label.grid(row = 1, column = 0) 
+
+username_label = Label(text = "Email/Username:")
+username_label.grid(row = 2, column = 0)  
+
+password_label = Label(text = "Password:") 
+password_label.grid(row = 3, column = 0)
+
+    # Entries
+    
+website_entry = Entry(width = 35)  
+website_entry.grid(row = 1, column = 1, columnspan = 2)
+website_entry.focus()
+
+username_entry = Entry(width = 35)
+username_entry.grid(row = 2, column = 1, columnspan = 2)
+username_entry.insert(END, "aadarshrai1801@gmail.com") 
+
+password_entry = Entry(width = 21)
+password_entry.grid(row = 3, column = 1)
+
+    # Button
+    
+generate_button = Button(text = "Generate Password", command = generate_password)
+generate_button.grid(row = 3, column = 2)    
+    
+add_button = Button(text = "Add", width = 36, command = save_password)  
+add_button.grid(row = 4, column = 1, columnspan = 2)  
+
 
 window.mainloop()
